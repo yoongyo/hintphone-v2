bc8c5e875f06fd5e12f14e4de22ed7832a344770
yoongyo <jyg0172@naver.com>
2021-07-15 17:17:06 +0900

    edit settings

--- ch1/mysite/settings/prod.py
+++ ch1/mysite/settings/prod.py
@@ -4,12 +4,27 @@ DEBUG = False
 ALLOWED_HOSTS = ['http://52.78.101.96:8000/']
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mysite', 'media')
+AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'hintphone'
 
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'mysite', 'staticfiles')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION_STATIC = 'static'
+AWS_LOCATION_MEDIA = 'media'
+
 STATICFILES_DIRS = [
- os.path.join(BASE_DIR,  'mysite', 'static'),
+    os.path.join(BASE_DIR, 'mysite/static'),
 ]
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION_STATIC)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION_MEDIA)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
